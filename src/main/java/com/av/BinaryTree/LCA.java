b78b0de31d077824d0f73765672547f15bc7a300
Anuj Vashisht <avas12@sapient.com>
2018-02-12 16:51:17 +0530

    added the LCA

---
+++ src/main/java/com/av/BinaryTree/LCA.java
@@ -0,0 +1,67 @@
+package com.av.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LCA {
+
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.left = new Node(2);
+        node.right = new Node(3);
+        node.left.left = new Node(4);
+        node.left.right = new Node(5);
+        node.left.left.left = new Node(7);
+        node.right.right = new Node(6);
+        node.right.right.left = new Node(8);
+
+        System.out.println("LCA "+findLCA(node,4,5));
+        System.out.println("LCA "+findLCA(node,4,6));
+    }
+
+
+    /*
+    * Traverse the path to both the nodes
+    * extract the node just before mismatch
+    * take care of nodes that are not in path
+    * */
+
+    public static int findLCA(Node root, int data,int data1){
+        List list = new ArrayList();
+        List list1 = new ArrayList();
+        int j = -1;
+
+        if(!findPathUtil(root,data,list)||!findPathUtil(root,data1,list1)){
+            System.out.println("data couldn't be find in the binary tree");
+            return j;
+        }
+        for (int i = 0; i < list.size() && i< list1.size() ; i++) {
+            if(list.get(i)!= list1.get(i)){
+                j=  (Integer) list.get(i-1);
+                break;
+            }
+
+        }
+        return j;
+    }
+
+    public static boolean  findPathUtil(Node node, int data, List list){
+        if(node == null){
+            return false;
+        }
+
+        list.add(node.data);
+
+        if(node.data == data){
+            return true;
+        }
+        if(node.left !=null && findPathUtil(node.left,data,list)){
+            return true;
+        }
+        if(node.right !=null && findPathUtil(node.right,data,list)){
+            return  true;
+        }
+         list.remove(list.size()-1);
+        return  false;
+    }
+}
